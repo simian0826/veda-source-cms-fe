554fe8321f18bb7419b4c87476c74a4ecc463efe
anotherlover <junqincq@deloitte.com.cn>
2024-09-26 17:03:22 +0800

    build: build

--- dist/assets/index.6fvgil5y.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./index.DOORBLvy.js";const o=t=>e.get({url:"/project/list",params:t}),c=t=>e.get({url:"/project/detail/".concat(t)}),p=t=>e.post({url:"/project/create",params:t}),s=t=>e.post({url:"/project/update",params:t}),u=t=>e.post({url:"/project/delete/".concat(t)});export{c as a,p as c,u as d,o as g,s as u};
---
+++ dist/assets/index.DFrYayqQ.js
@@ -0,0 +1,1 @@
+import{bg as o,bh as s,bi as a}from"./index.BVjvsxO0.js";const l=o(s),b=o(a);export{l as C,b as R};
