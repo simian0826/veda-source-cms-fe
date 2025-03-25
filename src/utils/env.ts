44f79e9c6029cf11fef5c3c95d71bfe6e7c2ae52
brunli <brunli@deloitte.com.cn>
2025-03-25 21:05:12 +0800

    fix: modify getAppEnvConfig

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -29,22 +29,43 @@ const getVariableName = (title: string) => {
     .replace(/\s/g, "");
 };
 
+// export function getAppEnvConfig() {
+//   const ENV_NAME = "_APP_CONFIG_";
+//   const ENV = import.meta.env.DEV
+//     ? // 开发环境下使用 import.meta.env
+//       (import.meta.env as unknown as GlobEnvConfig)
+//     : // 生产环境下使用 window._APP_CONFIG_
+//       (window[ENV_NAME] as unknown as GlobEnvConfig);
+
+//   const {
+//     VITE_GLOB_APP_TITLE = "VEDA SOURCE CMS", // 提供默认值
+//     VITE_GLOB_API_URL_PREFIX = "/veda-source",
+//     VITE_GLOB_UPLOAD_URL = "",
+//   } = ENV || {};
+
+//   let { VITE_GLOB_API_URL = "" } = ENV || {};
+
+//   if (localStorage.getItem(API_ADDRESS)) {
+//     const address = JSON.parse(localStorage.getItem(API_ADDRESS) || "{}");
+//     if (address?.key) VITE_GLOB_API_URL = address?.val;
+//   }
+
+//   return {
+//     VITE_GLOB_APP_TITLE,
+//     VITE_GLOB_API_URL,
+//     VITE_GLOB_API_URL_PREFIX,
+//     VITE_GLOB_UPLOAD_URL,
+//   };
+// }
+
 export function getAppEnvConfig() {
-  const ENV_NAME = getVariableName(import.meta.env.VITE_GLOB_APP_TITLE);
-  const ENV = import.meta.env.DEV
-    ? // Get the global configuration (the configuration will be extracted independently when packaging)
-      (import.meta.env as unknown as GlobEnvConfig)
-    : (window[ENV_NAME] as unknown as GlobEnvConfig);
   const {
     VITE_GLOB_APP_TITLE,
+    VITE_GLOB_API_URL,
     VITE_GLOB_API_URL_PREFIX,
     VITE_GLOB_UPLOAD_URL,
-  } = ENV;
-  let { VITE_GLOB_API_URL } = ENV;
-  if (localStorage.getItem(API_ADDRESS)) {
-    const address = JSON.parse(localStorage.getItem(API_ADDRESS) || "{}");
-    if (address?.key) VITE_GLOB_API_URL = address?.val;
-  }
+  } = import.meta.env;
+
   return {
     VITE_GLOB_APP_TITLE,
     VITE_GLOB_API_URL,
